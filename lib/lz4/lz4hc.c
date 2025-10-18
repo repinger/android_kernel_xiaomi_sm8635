3ec2fdba27daa897fdcbc813d25bc60b5988a082
chandu078 <chandudyavanapelli03@gmail.com>
2025-10-18 17:47:40 +0300

    treewide: guard MIN/MAX macros with #ifndef
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- lib/lz4/lz4hc.c
+++ lib/lz4/lz4hc.c
@@ -77,9 +77,12 @@ typedef enum { noDictCtx, usingDictCtxHc } dictCtx_directive;
 
 
 /*===   Macros   ===*/
+#ifndef MIN
 #define MIN(a,b)   ( (a) < (b) ? (a) : (b) )
+#endif
+#ifndef MAX
 #define MAX(a,b)   ( (a) > (b) ? (a) : (b) )
-
+#endif
 
 /*===   Levels definition   ===*/
 typedef enum { lz4mid, lz4hc, lz4opt } lz4hc_strat_e;
